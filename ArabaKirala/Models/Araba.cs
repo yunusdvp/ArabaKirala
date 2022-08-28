2759d7c937667b2fd5b313135c964e7004c5e8dc
Yunus Emre ÖZŞAHİN <yunusemre7033@gmail.com>
2022-08-28 22:53:48 +0300

    son değişiklikler

---
+++ ArabaKirala/Models/Araba.cs
@@ -0,0 +1,10 @@
+﻿namespace ArabaKirala.Models
+{
+    public class Araba
+    {
+        public int id { get; set; }
+        public String marka { get; set; }
+        public int Model { get; set; }
+        public int GünlükKira { get; set; }
+    }
+}
---
+++ ArabaKirala/Models/Musteri.cs
@@ -0,0 +1,11 @@
+﻿namespace ArabaKirala.Models
+{
+    public class Musteri
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string email { get; set; }
+
+
+    }
+}
